9cd730c12c35a2f900e7596f18b44127d56e4b8f
Arunesh kumar <aruneshkumar.king@gmail.com>
2022-12-24 22:29:44 +0530

    7.1 : user data displaying on dashboard

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,16 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Grid, Typography, Button, CssBaseline } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import ChangePassword from "./auth/ChangePassword";
-import { removeToken } from "../services/localStorageServices";
+import { getToken, removeToken } from "../services/localStorageServices";
 import { useDispatch } from "react-redux";
 import { unSetUserToken } from "../features/authSlice";
+import { useGetLoggedUserQuery } from "../services/userAuthApi";
 
 const Dashboard = () => {
+    const [userData, setUserData] = useState({
+        email: "",
+        name: "",
+    });
+
+    //!     Access Token
+    const { access_token } = getToken();
+
+    //!     Redirect Navigation
     const navigate = useNavigate();
 
+    //!     RTK Query
+    const { data, isSuccess } = useGetLoggedUserQuery(access_token);
+    console.log(data);
+
+    useEffect(() => {
+        if (data && isSuccess) {
+            setUserData({
+                email: data.email,
+                name: data.name,
+            });
+        }
+    }, [data, isSuccess]);
+
+    //!     Redux
     const dispatch = useDispatch();
 
+    //!     Logout Handler
     const logoutHandler = () => {
         dispatch(unSetUserToken({ access_token: null }));
 
@@ -24,8 +49,8 @@ const Dashboard = () => {
             <CssBaseline />
             <Grid container>
                 <Grid item sm={4} sx={{ backgroundColor: "gray", p: 5, color: "white" }}>
-                    <Typography variant="h4">Name : Arunesh kumar</Typography>
-                    <Typography variant="h6">Email : aruneshkumar.king@gmail.com</Typography>
+                    <Typography variant="h4">Name : {userData.name}</Typography>
+                    <Typography variant="h6">Email : {userData.email}</Typography>
                     <Button variant="contained" color="warning" size="large" sx={{ mt: 8 }} onClick={logoutHandler}>
                         Logout
                     </Button>
--- client/src/services/userAuthApi.js
+++ client/src/services/userAuthApi.js
@@ -31,7 +31,20 @@ export const userAuthApi = createApi({
                 };
             },
         }),
+
+        //!        Profile User
+        getLoggedUser: builder.query({
+            query: (access_token) => {
+                return {
+                    url: "profile/",
+                    method: "GET",
+                    headers: {
+                        authorization: `Bearer ${access_token}`,
+                    },
+                };
+            },
+        }),
     }),
 });
 
-export const { useRegisterUserMutation, useLoginUserMutation } = userAuthApi;
+export const { useRegisterUserMutation, useLoginUserMutation, useGetLoggedUserQuery } = userAuthApi;
