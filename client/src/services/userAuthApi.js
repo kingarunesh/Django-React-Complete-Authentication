938145c3289ae9133607c314ad078ed6e5d59d08
Arunesh kumar <aruneshkumar.king@gmail.com>
2022-12-24 12:51:50 +0530

    3.1 : login endpoint setup

--- client/src/services/userAuthApi.js
+++ client/src/services/userAuthApi.js
@@ -4,6 +4,7 @@ export const userAuthApi = createApi({
     reducerPath: "userAuthApi",
     baseQuery: fetchBaseQuery({ baseUrl: "http://127.0.0.1:8000/api/user/" }),
     endpoints: (builder) => ({
+        //!     Register user
         registerUser: builder.mutation({
             query: (user) => {
                 return {
@@ -16,7 +17,21 @@ export const userAuthApi = createApi({
                 };
             },
         }),
+
+        //!        Login User
+        loginUser: builder.mutation({
+            query: (user) => {
+                return {
+                    url: "login/",
+                    method: "POST",
+                    body: user,
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                };
+            },
+        }),
     }),
 });
 
-export const { useRegisterUserMutation } = userAuthApi;
+export const { useRegisterUserMutation, useLoginUserMutation } = userAuthApi;
