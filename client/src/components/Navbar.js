923d8191203674f4c0319068468e7b247c2bd987
Arunesh kumar <aruneshkumar.king@gmail.com>
2022-12-24 14:41:19 +0530

    5.1 : navbar menu hide and show

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,8 +1,11 @@
 import React from "react";
 import { Box, AppBar, Toolbar, Typography, Button } from "@mui/material";
 import { NavLink } from "react-router-dom";
+import { getToken } from "../services/localStorageServices";
 
 const Navbar = () => {
+    const { access_token } = getToken();
+
     const activePage = ({ isActive }) => {
         return { backgroundColor: isActive ? "#004182" : "" };
     };
@@ -15,15 +18,34 @@ const Navbar = () => {
                         <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
                             Classimax
                         </Typography>
+
                         <Button component={NavLink} to="/" sx={{ color: "white", textTransform: "none", fontSize: "1rem" }} style={activePage}>
                             Home
                         </Button>
+
                         <Button component={NavLink} to="/contact" sx={{ color: "white", textTransform: "none", fontSize: "1rem" }} style={activePage}>
                             Contact
                         </Button>
-                        <Button component={NavLink} to="/login" sx={{ color: "white", textTransform: "none", fontSize: "1rem" }} style={activePage}>
-                            Login
-                        </Button>
+
+                        {access_token ? (
+                            <Button
+                                component={NavLink}
+                                to="/dashboard"
+                                sx={{ color: "white", textTransform: "none", fontSize: "1rem" }}
+                                style={activePage}
+                            >
+                                Dashboard
+                            </Button>
+                        ) : (
+                            <Button
+                                component={NavLink}
+                                to="/login"
+                                sx={{ color: "white", textTransform: "none", fontSize: "1rem" }}
+                                style={activePage}
+                            >
+                                Login
+                            </Button>
+                        )}
                     </Toolbar>
                 </AppBar>
             </Box>
