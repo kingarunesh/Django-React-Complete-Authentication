96f8ad96a307cf138bd6f8143d12860ec80b78e8
Arunesh kumar <aruneshkumar.king@gmail.com>
2022-12-24 19:57:28 +0530

    6.1 : auth slice created

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/dist/query";
 import { userAuthApi } from "../services/userAuthApi";
+import authReducer from "../features/authSlice";
 
 export const store = configureStore({
     reducer: {
         [userAuthApi.reducerPath]: userAuthApi.reducer,
+        auth: authReducer,
     },
 
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(userAuthApi.middleware),
